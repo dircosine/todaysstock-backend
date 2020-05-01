a666852f86f4a66ed40227d39f247891960f2b37
dircosine <zidhdy@gmail.com>
2020-05-01 20:25:36 +0900

    Refactor for Heroku deploy

--- src/server.ts
+++ src/server.ts
@@ -4,23 +4,30 @@ import depthLimit from 'graphql-depth-limit';
 import { createServer } from 'http';
 import cors from 'cors';
 import schema from './schema';
+import './env';
 
 import { updateTodaysInfos } from './routes/tournament';
+
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)],
+  playground: true,
 });
 
 app.use('*', cors());
 server.applyMiddleware({ app, path: '/graphql' });
 
+app.get('/', (req, res) => {
+  res.send('호잇!');
+});
 app.get('/update', updateTodaysInfos);
 
 const httpServer = createServer(app);
-httpServer.listen({ port: 4000 }, (): void =>
+httpServer.listen({ port: PORT }, (): void =>
   console.log(
-    `\n🚀      GraphQL is now running on http://localhost:4000/graphql`,
+    `\n🚀      GraphQL is now running on http://localhost:${PORT}/graphql`,
   ),
 );
